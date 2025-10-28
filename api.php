aca61ed50f4a27a644dba6afe11ea23677919730
SoloPatryk <157973025+SoloPatryk@users.noreply.github.com>
2025-10-28 21:51:55 +0100

    Now using localstorage to store individual users clicks

--- api.php
+++ api.php
@@ -3,7 +3,7 @@ header('Content-Type: application/json');
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     // Reading from clicks variable, and returning it to user
-    print json_encode('3');
+    print json_encode(rand(1, 100));
 } else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Adding to clicks variable
     print json_encode('ok');
