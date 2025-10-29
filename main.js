109707d85607f0e8987d7d115bc7f9a5c11ff68c
SoloPatryk <zaorski.patryk.0@gmail.com>
2025-10-29 18:56:52 +0100

    Bug fixes and optimizations

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const API = 'api.php';
-
 const userLocale = navigator.language || 'pl-PL';
 const formatter = new Intl.NumberFormat(userLocale);
 const globalCounter = document.getElementById('globalCounter');
@@ -10,11 +9,17 @@ let globalClickCount = 0;
 let localClickCount = 0;
 
 function updateLocalCounterText() {
+	localCounter.textContent = 'Your clicks: ' + formatter.format(localClickCount);
+}
+function updateGlobalCounterText() {
 	// Antycheat, not too great but enough for kids changing the random values in F12
 	if (globalClickCount < localClickCount) {
 		localClickCount = 0;
-	}
-	localCounter.textContent = 'Your clicks: ' + formatter.format(localClickCount);
+		updateLocalCounterText();
+		localStorage.setItem('localClickCount', localClickCount.toString());
+	} // had to remove it, it was reseting progress on page reload, and i dont want ppl to lose progress :3
+	// nwm my bad, its fine I just put it in wrong function
+	globalCounter.textContent = 'All time clicks: ' + globalClickCount;
 }
 
 button.addEventListener('click', async () => {
@@ -25,11 +30,9 @@ button.addEventListener('click', async () => {
 		const response = await fetch(API, { method: 'POST' });
 		if (!response.ok) {
 			console.error('HTTP error:', response.status);
-			return;
 		}
-		const data = await response.json();
-		globalClickCount = parseInt(data);
-		updateGlobalClicks();
+		globalClickCount++;
+		updateGlobalCounterText();
 	} catch (error) {
 		console.error('Failed to register click: ', error);
 	}
@@ -38,23 +41,24 @@ button.addEventListener('click', async () => {
 async function updateGlobalClicks() {
 	try {
 		const response = await fetch(API, { method: 'GET' });
-
 		if (!response.ok) {
 			console.error('HTTP error:', response.status);
 			return;
 		}
-
 		const data = await response.json();
 		globalClickCount = parseInt(data);
-		globalCounter.textContent = 'All time clicks: ' + globalClickCount;
+		updateGlobalCounterText();
 	} catch (error) {
 		console.error('Fetch error:', error);
 	}
 }
 
 setInterval(updateGlobalClicks, 5000);
-updateGlobalClicks();
 
-const saved = localStorage.getItem('localClickCount');
-localClickCount = saved ? parseInt(saved) : 0;
-updateLocalCounterText();
+window.addEventListener('load', async () => {
+	updateGlobalClicks();
+
+	const saved = localStorage.getItem('localClickCount');
+	localClickCount = saved ? parseInt(saved) : 0;
+	updateLocalCounterText();
+});
