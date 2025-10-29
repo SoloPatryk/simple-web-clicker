ba52477d46ad232c7722d62e55e6f957c52155ec
SoloPatryk <157973025+SoloPatryk@users.noreply.github.com>
2025-10-29 22:54:09 +0100

    Added comments (yes really :3)

--- api.php
+++ api.php
@@ -1,13 +1,26 @@
 <?php
+// REQUIRES php-apcu PACKAGE INSTALLED TO WORK CORRECTLY!!!
+
+// I dont need to explain this, dont I? :3
 header('Content-Type: application/json');
+
+// Fetching the clicks from memory
 $clicks = apcu_fetch('clicks') ?: 0;
 
+// Checking if its get or post
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    // Reading from clicks variable, and returning it to user
+    // Reading from clicks variable, and returning it
     print json_encode($clicks);
+
+    // Retuning ok and quitting
+    http_response_code(200);
+    exit;
 } else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Adding to clicks variable
+    // Adding to clicks variable, and storing it in memory
     $clicks++;
     apcu_store('clicks', $clicks);
-    print json_encode($clicks);
+
+    // Retuning ok and quitting
+    http_response_code(200);
+    exit;
 }
--- main.js
+++ main.js
@@ -1,36 +1,53 @@
-const API = 'api.php';
+// api path
+const api = 'api.php';
+
+// Trying to get users locale and display numbers in it, as fallback use polish (100 000)
 const userLocale = navigator.language || 'pl-PL';
 const formatter = new Intl.NumberFormat(userLocale);
+
+// getting the Text boxes and button
 const globalCounter = document.getElementById('globalCounter');
 const localCounter = document.getElementById('localCounter');
 const button = document.getElementById('button');
 
+// declaring the variables
 let globalClickCount = 0;
 let localClickCount = 0;
 
+// Updating the global clicks text box
 function updateLocalCounterText() {
 	localCounter.textContent = 'Your clicks: ' + formatter.format(localClickCount);
 }
+
+// Updating the local clicks text box
 function updateGlobalCounterText() {
-	// Antycheat, not too great but enough for kids changing the random values in F12
+	// Antycheat, not too great but enough for kids changing the random values in debugger
+	// Checks if the value of clicks by everyone is smaller than local click count,
+	// if its true it resets local clicks to 0
 	if (globalClickCount < localClickCount) {
 		localClickCount = 0;
 		updateLocalCounterText();
 		localStorage.setItem('localClickCount', localClickCount.toString());
-	} // had to remove it, it was reseting progress on page reload, and i dont want ppl to lose progress :3
-	// nwm my bad, its fine I just put it in wrong function
+	}
+
 	globalCounter.textContent = 'All time clicks: ' + globalClickCount;
 }
 
+// Logic on button press
 button.addEventListener('click', async () => {
 	try {
+		// Updating the local count
 		localClickCount++;
 		updateLocalCounterText();
 		localStorage.setItem('localClickCount', localClickCount.toString());
-		const response = await fetch(API, { method: 'POST' });
+
+		// Updating the global count
+		const response = await fetch(api, { method: 'POST' });
 		if (!response.ok) {
 			console.error('HTTP error:', response.status);
 		}
+
+		// Adding 1 to global click, instead of refetching the data from server every click
 		globalClickCount++;
 		updateGlobalCounterText();
 	} catch (error) {
@@ -38,9 +55,10 @@ button.addEventListener('click', async () => {
 	}
 });
 
+// Logic to update fetch the global clicks from server
 async function updateGlobalClicks() {
 	try {
-		const response = await fetch(API, { method: 'GET' });
+		const response = await fetch(api, { method: 'GET' });
 		if (!response.ok) {
 			console.error('HTTP error:', response.status);
 			return;
@@ -53,11 +71,15 @@ async function updateGlobalClicks() {
 	}
 }
 
+// Setting the updating glbal click to run every 5 sec
 setInterval(updateGlobalClicks, 5000);
 
+// Things to do after a page has been loaded
 window.addEventListener('load', () => {
+	// Updating global clicks, so it doesnt show 0 for first 5 seconds
 	updateGlobalClicks();
 
+	// Reading the local clicks from browser memory, if it exists
 	const saved = localStorage.getItem('localClickCount');
 	localClickCount = saved ? parseInt(saved) : 0;
 	updateLocalCounterText();
