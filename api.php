b038cae7271f280e23a92bc5dc5540336246953d
SoloPatryk <157973025+SoloPatryk@users.noreply.github.com>
2025-10-29 15:54:19 +0100

    Added actual counting, with backend sync

--- api.php
+++ api.php
@@ -1,10 +1,13 @@
 <?php
 header('Content-Type: application/json');
+$clicks = apcu_fetch('clicks') ?: 0;
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     // Reading from clicks variable, and returning it to user
-    print json_encode(rand(1, 100));
+    print json_encode($clicks);
 } else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Adding to clicks variable
-    print json_encode('ok');
+    $clicks++;
+    apcu_store('clicks', $clicks);
+    print json_encode($clicks);
 }
