380f6b7e345a4c0f502179668af783a5201fac37
SoloPatryk <zaorski.patryk.0@gmail.com>
2025-10-29 15:54:19 +0100

    Added actual counting, with backend sync

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const API = 'api.php';
 
 const userLocale = navigator.language || 'pl-PL';
+const formatter = new Intl.NumberFormat(userLocale);
 const globalCounter = document.getElementById('globalCounter');
 const localCounter = document.getElementById('localCounter');
 const button = document.getElementById('button');
@@ -9,14 +10,18 @@ let globalClickCount = 0;
 let localClickCount = 0;
 
 function updateLocalCounterText() {
-	const formatter = new Intl.NumberFormat(userLocale);
 	localCounter.textContent = 'Your clicks: ' + formatter.format(localClickCount);
 }
 
-button.addEventListener('click', () => {
+button.addEventListener('click', async () => {
 	localClickCount++;
 	updateLocalCounterText();
 	localStorage.setItem('localClickCount', localClickCount.toString());
+	const response = await fetch(API, { method: 'POST' });
+	if (!response.ok) {
+		console.error('HTTP error:', response.status);
+		return;
+	}
 });
 
 async function updateGlobalClicks() {
@@ -29,7 +34,12 @@ async function updateGlobalClicks() {
 		}
 
 		const data = await response.json();
-		globalCounter.textContent = 'All time clicks: ' + data;
+		globalClickCount = parseInt(data);
+		globalCounter.textContent = 'All time clicks: ' + globalClickCount;
+		// Antycheat, not too great but enough for kids changing the random values in F12
+		if (globalClickCount > localClickCount) {
+			localClickCount = 0;
+		}
 	} catch (error) {
 		console.error('Fetch error:', error);
 	}
