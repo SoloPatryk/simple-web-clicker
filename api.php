9b5821ee7003d66f11d9d1ea2f70643bdc55a552
SoloPatryk <157973025+SoloPatryk@users.noreply.github.com>
2025-12-26 01:18:40 +0100

    Made the api without acpu by default, and made acpu an option

--- api.php
+++ api.php
@@ -4,31 +4,18 @@
 // I dont need to explain this, dont I? :3
 header('Content-Type: application/json');
 
-// Fetching the clicks from memory, or file if its not found in memory
-$clicks = apcu_fetch('clicks') ?:
-    ((is_file('clicks') ? (int) file_get_contents('clicks') : 0) ?:
-        0);
-
-// Fetching last saved date from memory
-$lastSave = apcu_fetch('lastSave') ?: 0;
-$time = time();
+// Fetching the clicks from file
+$clicks = (int) file_get_contents('clicks');
 
 // Checking if its get or post
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     // Reading from clicks variable, and returning it
     print json_encode($clicks);
 } else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Adding to clicks variable, and storing it in memory
+    // Adding to clicks variable and saving it to file
     $clicks++;
-    apcu_store('clicks', $clicks);
     echo json_encode($clicks);
-}
-
-// Checking if it has passed 5 min,
-// if yes save to file, and update last saved date
-if ($time - $lastSave >= 300) {
     file_put_contents('clicks', $clicks, LOCK_EX);
-    apcu_store('lastSave', time());
 }
 
 // Retuning ok and quitting
